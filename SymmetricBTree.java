5615907b2bce63b85ebf5312f779867cf9224529
kapalani <kavinpalni@gmail.com>
2014-09-22 00:41:42 -0400

    Added iterative coding
    Added iterative coding

--- SymmetricBTree.java
+++ SymmetricBTree.java
@@ -13,7 +13,7 @@ public class SymmetricBTree{
         System.out.println(result);
     }
     
-    public static boolean isSymmetric(TreeNode root){
+    public static boolean isSymmetricRecursive(TreeNode root){
         if(root==null)
             return true;
         return helper(root.left, root.right);
@@ -25,6 +25,31 @@ public class SymmetricBTree{
         if(left==null||right==null)
             return false;
         return left.val==right.val && helper(left.left, right.right) && helper(left.right, right.left);
+    }
+	
+	public static boolean isSymmetric(TreeNode root){
+        if(root==null||(root.left==null && root.right==null))
+            return true;
+        Stack<TreeNode> s1 = new Stack<TreeNode>();
+        Stack<TreeNode> s2 = new Stack<TreeNode>();
+        s1.push(root.left);
+        s2.push(root.right);
+        
+        while(!s1.isEmpty() && !s2.isEmpty()){
+            TreeNode a = s1.pop();
+            TreeNode b = s2.pop();
+            if(a==null && b==null)    
+                continue;
+            if(a==null || b==null)
+                return false;
+            if(a.val!=b.val)
+                return false;
+            s1.push(a.left);
+            s2.push(b.right);
+            s1.push(a.right);
+            s2.push(b.left);
+        }
+       return true;
     }
 }
 
